4b4b0b039441170c396b2cc5461adf4e26603099
João Fernandes <ojoaofernandes@gmail.com>
2018-03-12 19:47:05 -0300

    Adds connection to the database and instantiates the repository.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 const Consumer = require('./transports/Consumer');
+const Repository = require('./database/Repository');
+const MongoDB = require('./database/MongoDB');
 
-// Defines the consumer configuration object with the address of the
-// streaming platform.
-const address = {
-  ip: '127.0.0.1',
-  port: 2181,
+// Defines the configuration object with the addresses of the streaming
+// platform and the database.
+const config = {
+  database: {
+    ip: '127.0.0.1',
+    port: 27017,
+    database: 'cryptosense-tickers',
+  },
+  streaming: {
+    ip: '127.0.0.1',
+    port: 2181,
+  },
 };
 
-const consumer = new Consumer(address);
+// Instantiates a new repository based on a MongoDB connection.
+const mongo = new MongoDB(config.database);
+const repo = new Repository(mongo);
+
+// Instantiates a new streaming platform consumer.
+const consumer = new Consumer(config.streaming);
 
 // Subscribes to the streaming platform topic to receive price updates.
 consumer.listen({
   name: 'price.update',
-  offset: 80,
+  offset: 0,
   partition: 0,
 });
 
